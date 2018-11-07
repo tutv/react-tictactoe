cd19f8b6d709eb22ad7eed8fdf34fff9c6b446e8
Max <tutv95@gmail.com>
2018-11-08 01:08:33 +0700

    Updates

---
+++ precache-manifest.0623f66da71e10eebc4d62034fa2ed11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab1576f65a00b9e0d950",
+    "url": "/static/js/main.ab1576f6.chunk.js"
+  },
+  {
+    "revision": "3a4e6a9c12f63367deb3",
+    "url": "/static/js/1.3a4e6a9c.chunk.js"
+  },
+  {
+    "revision": "ab1576f65a00b9e0d950",
+    "url": "/static/css/main.fe256da0.chunk.css"
+  },
+  {
+    "revision": "3b7bdf8497c679dae588080e1abdf748",
+    "url": "/index.html"
+  }
+];
